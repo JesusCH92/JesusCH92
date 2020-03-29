0bb30671f55c91cea1e8fb0000dd815f90d1237b
Jesus <jcocaharo@hotmail.com>
2020-03-29 01:50:29 +0100

    typo fix-> clean comments by old versions

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,22 +8,20 @@ class IndexController extends BaseController{
     public function indexAction(){
         $jobs = Job::all();     // *  ddbb
         $project1 = new Project('Project 1', 'Description 1');
-        $project1-> months = 16;
+        $project1-> months = 12;
         $projects = [
             $project1
         ];
         $limitMonth = 15;
         $fiterFunction = function (array $job) use ($limitMonth){
-            return $job['month'] >= $limitMonth;
+            return $job['months'] >= $limitMonth;
         };
 
         $jobs = array_filter($jobs->toArray(), $fiterFunction);
 
         $lastName = "CH";
         $name = "Jesús $lastName";
-        $limitMonths = 2000;
 
-        // include '../views/index.php';
         return $this->renderHTML('index.twig', [
             'name' => $name,
             'jobs' => $jobs,
