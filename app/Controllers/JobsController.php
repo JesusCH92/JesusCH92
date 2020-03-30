23b993ad2ad1eb53dc2a919ec4b6e88a5258304f
Jesus <jcocaharo@hotmail.com>
2020-03-30 05:14:17 +0200

    update my class for dependency inject

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,9 +3,32 @@
 namespace App\Controllers;
 
 use App\Models\Job;
+use App\Services\JobService;
 use Respect\Validation\Validator as v;
+use Zend\Diactoros\Response\RedirectResponse;
+use Zend\Diactoros\ServerRequest;
 
 class JobsController extends BaseController{
+    private $jobService;
+
+    public function __construct(JobService $jobService)
+    {
+        parent::__construct();
+        $this->jobService = $jobService;
+    }
+
+    public function indexAction() {
+        $jobs  = Job::withTrashed()->get();
+        return $this->renderHTML('Jobs/index.twig', compact('jobs'));
+    }
+
+    public function deleteAction (ServerRequest $request) {
+        $params = $request->getQueryParams();
+        $this->jobService->deleteJob($params['id']);
+
+        return new RedirectResponse('/primer-proyecto-php/jobs');
+    }
+
     public function getAddjobAction($request){
         $responseMessage=null;
         if($request->getMethod() == 'POST'){
@@ -38,7 +61,6 @@ class JobsController extends BaseController{
 
         }
 
-        // include '../views/addJob.php';
         return $this->renderHTML('addJob.twig',[
             'responseMessage' => $responseMessage 
         ]);
