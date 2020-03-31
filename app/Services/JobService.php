b0a85292a38676af24d342d3b4cf1891221da00a
Jesus <jcocaharo@hotmail.com>
2020-03-31 03:45:46 +0200

    update my JobService by emit an exception if I have an error

--- app/Services/JobService.php
+++ app/Services/JobService.php
@@ -9,7 +9,11 @@ use App\Models\Job;
 class JobService
 {
     public function deleteJob($id){
-        $job = Job::find($id);
+//        $job = Job::find($id);
+//        if (!$job) {
+//            throw new \Exception('Job not found');
+//        }
+        $job = Job::findOrFail($id); // si no encuentra el objeto lanzara el error, es lo mismo que el comentario de arriba
         $job->delete();
     }
 }
