25f8a8e02a941381cc0f15eb699b6bdb8bb914b3
Jesus <jcocaharo@hotmail.com>
2020-03-31 05:55:08 +0200

    doing test by use xDebug

--- app/Services/JobService.php
+++ app/Services/JobService.php
@@ -13,7 +13,6 @@ class JobService
 //        if (!$job) {
 //            throw new \Exception('Job not found');
 //        }
-        var_dump($this);
         $job = Job::findOrFail($id); // si no encuentra el objeto lanzara el error, es lo mismo que el comentario de arriba
         $job->delete();
     }
