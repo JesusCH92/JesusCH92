1dd98383b73a0a3114029f5dd42e27e4a2978a32
Jesus <jcocaharo@hotmail.com>
2020-04-01 07:41:02 +0200

    create a new command by create user

---
+++ app/Commands/CreateUserCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Commands;
+
+use App\Models\User;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class CreateUserCommand extends Command
+{
+    protected static $defaultName = 'app:create-user';
+    protected function configure()
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the user.')
+            ->addArgument('password', InputArgument::OPTIONAL, 'Your password')
+        ;
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'User Creator',
+            '============',
+            '',
+        ]);
+        $user = new User();
+        $user->email = $input->getArgument('email');
+
+        if ( $input->getArgument('password') ) {
+            $user->password = password_hash($input->getArgument('password'), PASSWORD_DEFAULT);
+        }
+
+        $user->password = password_hash('admin', PASSWORD_DEFAULT);
+
+        $user->save();
+        $output->writeln('Done.');
+
+        return 0;
+    }
+}
