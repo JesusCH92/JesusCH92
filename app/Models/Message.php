c432544e79a226cab07986bf1297cb68289b4c8f
Jesus <jcocaharo@hotmail.com>
2020-04-01 07:05:49 +0200

    create my model for message table

---
+++ app/Models/Message.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Message extends Model{
+
+    protected $table = 'messages';  // ? Nombre de nuestra tabla en nuestra bbdd
+
+
+}
