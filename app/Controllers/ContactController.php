f497ad24ab008971214821d12ea19782d67f58ae
Jesus <jcocaharo@hotmail.com>
2020-04-01 02:50:34 +0200

    implements my method send with Swift Mailet to send email

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -2,7 +2,11 @@
  
 namespace App\Controllers;
 
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
 use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\Response\RedirectResponse;
 
 class ContactController extends BaseController
 {
@@ -13,6 +17,26 @@ class ContactController extends BaseController
 
     public function send(ServerRequest $request)
     {
-        var_dump($request->getParsedBody());
+        $requestData = $request->getParsedBody();
+
+        $transport = (new Swift_SmtpTransport(getenv('SMTP_HOST'), getenv('SMTP_PORT')))
+            ->setUsername(getenv('SMTP_USER'))
+            ->setPassword(getenv('SMTP_PASSWORD'))
+        ;
+
+        // Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+
+        // Create a message
+        $message = (new Swift_Message('Wonderful Subject'))
+            ->setFrom(['contact@mail.com' => 'Contact Form'])
+            ->setTo(['jcocaharo@hotmail.com' => 'A name'])
+            ->setBody('Hi, you have a new message. Name: ' . $requestData['name'] 
+                . ' Email: ' . $requestData['email'] . ' Message: ' . $requestData['message'])
+        ;
+
+        // Send the message
+        $result = $mailer->send($message);
+        return new RedirectResponse('/primer-proyecto-php/contact');
     }
 }
